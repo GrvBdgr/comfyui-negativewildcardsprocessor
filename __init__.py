f562bbd6d6389bd82bb22d4874b3e7c55756ee0a
GrvBdgr <mullerdanitrash@gmail.com>
2024-11-15 20:31:51 +0100

    Update __init__.py with custom token processor node

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .negative_wildcard_processor import NegativeWildcardProcessor
-NODE_CLASS_MAPPINGS = { "neg_wildcard_processor" : NegativeWildcardProcessor }
-NODE_DISPLAY_NAME_MAPPINGS = { "neg_wildcard_processor" : "Negative Wildcard Processor" }
+from .negative_wildcard_processor import NegativeWildcardProcessor, CustomTokenPreprocessor
+NODE_CLASS_MAPPINGS = { "neg_wildcard_processor" : NegativeWildcardProcessor, "custom_token_processor" : CustomTokenPreprocessor }
+NODE_DISPLAY_NAME_MAPPINGS = { "neg_wildcard_processor" : "Negative Wildcard Processor", "custom_token_processor" : "Custom Token Processor" }
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
